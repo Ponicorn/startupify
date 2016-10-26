d1bcbcaef2ebf982db499e4033e6de9a6490b647
Christophe Sauder <christophe.sauder@univ-lorraine.fr>
2016-10-26 12:28:05 +0200

    deny of one view, menu is add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="app">
-        <rngComponent></rngComponent>
+        <div id="rngComponentWrapper" class="rngComponentWrapper" :class="{ showMenu: showMenu }">
+            <rngComponent></rngComponent>
+            <div class="smallCuteLittleButton" v-on:click="showMenu = !showMenu"><div></div></div>
+        </div>
+        <menuGeneral></menuGeneral>
     </div>
 </template>
 
 <script>
 import bgFilter from './components/BgFilter.vue'
-import textImage from './components/TextImage.vue'
+// import textImage from './components/TextImage.vue' // not OK ATM
 import bobRoss from './components/BobRoss.vue'
-const componentList = [textImage, bobRoss, bgFilter];
+import menuGeneral from './components/MenuGeneral.vue'
+const componentList = [bobRoss, bgFilter];
 
 // Function to grab param from the url, from stackoverflow
 const getUrlParameter = (name) => {
@@ -25,9 +30,6 @@ switch (styleParam) {
     case 'bobross':
         rngComponent = bobRoss;
         break;
-    case 'textimage':
-        rngComponent = textImage;
-        break;
     case 'bgfilter':
         rngComponent = bgFilter;
         break;
@@ -37,8 +39,13 @@ switch (styleParam) {
 
 export default {
     name: 'app',
+    data() {
+        return  {
+            showMenu : false
+        }
+    },
     components: {
-        rngComponent
+        rngComponent, menuGeneral
     }
 }
 </script>
@@ -48,11 +55,51 @@ export default {
 
 html,
 body,
-.app {
+.app,
+.rngComponentWrapper {
     overflow: hidden;
     height: 100%;
     width: 100%;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+}
+
+.rngComponentWrapper {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background: #ccc;
+    transition: 0.5s;
+    z-index: 10;
+    .smallCuteLittleButton {
+        position: fixed;
+        bottom: 25px;
+        left: 25px;
+        height: 30px;
+        width:  30px;
+        z-index: 10;
+        cursor: pointer;
+        background: #ECECEC;
+        &>div {
+            transition: 0.5s;
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            background: #1f1f1f;
+            height: 20px;
+            width:  20px;
+        }
+    }
+
+    &.showMenu {
+        box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+        bottom: 100px;
+        left  : 100px;
+
+        .smallCuteLittleButton>div {
+            bottom: 10px;
+            left: 10px;
+        }
+    }
 }
 </style>
---
+++ src/components/MenuGeneral.vue
@@ -0,0 +1,73 @@
+<template>
+	<div id="menu" class="menu">
+		<div class="ListHolder">
+			<a href="/?style=bobross" class="item"><img src="img/bobross.gif" alt="bob ross" /></a>
+			<a href="/?style=bgfilter" class="item"><div class="miniBgFilter"><div>bla bla</div></div></a>
+
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+.menu {
+	position: fixed;
+	background: #ECECEC;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+
+	.ListHolder {
+		height: 100px;
+		width: calc(100% - 100px);
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		display: flex;
+
+		.item {
+			height: 100px;
+			min-width: 100px;
+			padding: 20px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			text-align: center;
+			cursor: pointer;
+			margin-right: 10px;
+			text-decoration: none;
+			color: inherit;
+			&:last-child {
+				margin-right: 0;
+			}
+			img {
+				max-width: 100%;
+				max-height: 100%;
+			}
+			.miniBgFilter {
+				background: #2c2c2c;
+				height: 100%;
+				width: 100%;
+				padding: 15px 10px;
+				&>div {
+					height: 100%;
+					width: 100%;
+					border: solid 2px #ececec;
+					color: #ececec;
+					display: flex;
+					text-align: center;
+					align-items: center;
+					justify-content: center;
+					font-size: 8px
+				}
+			}
+		}
+	}
+
+}
+</style>

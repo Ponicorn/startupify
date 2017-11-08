74bb215ca789881bdf0233665d8bc2be1abb0533
Christophe Sauder <christophe.sauder@univ-lorraine.fr>
2017-11-08 10:01:23 +0100

    test config pour deploy

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -53,7 +53,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: 'index.html',
+      template: 'index.dev.html',
       inject: true,
       minify: {
         removeComments: true,
